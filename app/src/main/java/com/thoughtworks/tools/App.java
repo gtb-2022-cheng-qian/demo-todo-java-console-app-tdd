b638520ea9ed036406b6b63849cc16cf57156dde
alancq98 <chengqian111222@163.com>
2022-01-14 17:30:55 +0800

    refactor: extract constants class

--- app/src/main/java/com/thoughtworks/tools/App.java
+++ app/src/main/java/com/thoughtworks/tools/App.java
@@ -5,17 +5,17 @@ package com.thoughtworks.tools;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
 
 public class App {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        throw new UnsupportedOperationException();
     }
 
     public List<String> run() {
         try {
-            return Files.readAllLines(Path.of("C:\\Users\\hp\\.todo\\tasks"));
+            return Files.readAllLines(Constants.TASK_FILE_PATH);
         } catch (IOException e) {
             throw new todoCannotReadFileException();
         }
