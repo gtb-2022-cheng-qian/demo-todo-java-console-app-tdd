c500f16cbfd3e5a7cb0b98f4c27aaf7ec77b6ec6
alancq98 <chengqian111222@163.com>
2022-01-18 14:12:06 +0800

    refactor: converted TaskMarshaller static methods to instance methods

--- app/src/main/java/com/thoughtworks/tools/TaskMarshaller.java
+++ app/src/main/java/com/thoughtworks/tools/TaskMarshaller.java
@@ -1,17 +1,15 @@
 package com.thoughtworks.tools;
 
 public class TaskMarshaller {
-    private TaskMarshaller() {
-    }
 
-    static String marshal(Task task) {
-        return "+ " + task.getName();
-    }
-
-    static Task unmarshal(int id, String line) {
+    Task unmarshal(int id, String line) {
         final var fields = line.split(" ", 2);//不包括+和x标志
         final var name = fields[1];
         final boolean isCompleted = fields[0].equals("x");
         return new Task(id, name, isCompleted);
+    }
+
+    String marshal(Task task) {
+        return "+ " + task.getName();
     }
 }
--- app/src/test/java/com/thoughtworks/tools/TaskFactoryTest.java
+++ app/src/test/java/com/thoughtworks/tools/TaskFactoryTest.java
@@ -1,19 +1,28 @@
 package com.thoughtworks.tools;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TaskFactoryTest {
+
+    private TaskMarshaller taskMarshaller;
+
+    @BeforeEach
+    void setUp() {
+        taskMarshaller = new TaskMarshaller();
+    }
+
     // Unit test
     @Test
     void should_parse_completed_property_for_task() {
-        final var isCompleted = TaskMarshaller.unmarshal(1, "+ task 01").isCompleted();
+        final var isCompleted = taskMarshaller.unmarshal(1, "+ task 01").isCompleted();
         Assertions.assertFalse(isCompleted);
     }
 
     @Test
     void should_support_name_with_multiple_white_spaces() {
-        final var task = TaskMarshaller.unmarshal(1, "+    foo   bar   ");
+        final var task = taskMarshaller.unmarshal(1, "+    foo   bar   ");
         Assertions.assertEquals("   foo   bar   ",task.getName());
     }
 }
